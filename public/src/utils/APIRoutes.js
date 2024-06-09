a1f4741756c91af11bbead6dedfac86b01e9570d
kholiavkoi <kholiavkoi@gmail.com>
2024-06-09 21:07:03 +0200

    changed host

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5001";
+export const host = "https://chat-app-api-hpxq.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
