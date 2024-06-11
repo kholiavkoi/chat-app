7add64976fc714f32f8f59cc3bb5e43534954a7d
kholiavkoi <kholiavkoi@gmail.com>
2024-06-11 16:15:33 +0200

    added env

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chat-app-server-a3v5.onrender.com";
+export const host = process.env.HOST;
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
