f568f9b7c646758f0ae495ac87d932e9c90bd926
kholiavkoi <kholiavkoi@gmail.com>
2024-06-11 16:23:39 +0200

    fixed env problem

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = process.env.HOST;
+export const host = process.env.REACT_APP_HOST;
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
